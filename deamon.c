e11a45d1ee0bc1f23dbb227386269d5ab705c9fe
linnabrown <lehuang@unc>
2020-01-09 00:53:12 -0500

    first add

---
+++ deamon.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+// #include <signal.h>
+// #include <sys/param.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include<string.h>
+
+// #include <time.h>
+
+int main(int argc, char* argv[]){
+    // FILE *fp = NULL;
+    pid_t process_id = 0;
+    pid_t sid = 0;
+    //create child process
+    process_id = fork();
+    //Indication of fork() failue
+    if ( process_id <0){
+        printf("fork failed!\n");
+        //return failure in exit status
+        exit(1);
+    }
+    //Parent process. Need to kill it.
+    if (process_id > 0){
+        printf("process_id of child process %d \n", process_id);
+        //return success in exit status
+        exit(0);
+    }
+    //unmask the file mode
+    umask(0);
+    //set new session
+    sid = setsid();
+    if(sid < 0){
+        // return failure
+        exit(1);
+    }
+    //change the current working directory to test_folder
+    chdir("/pine/scr/l/e/lehuang/");
+    //Close stdin. stdout and stderr
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+    //open a log file in write mode
+    // fp = fopen("log.txt", "w+");
+    while(1)
+    {
+        sleep(1440); // 1 day
+        // fprintf(fp,"Logging info .. \n");
+        // fflush(fp);
+        //implement and call some function that does work for this daemon.
+        system("find /pine/scr/l/e/lehuang/ -exec touch {} \\;");
+    }
+    // fclose(fp);
+    return(0);
+
+}
+
+
